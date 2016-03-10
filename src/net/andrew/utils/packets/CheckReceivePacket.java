07c77eabc5d73ed6418390c011211384c60a8dd1
xXAndrew28Xx <andrewtran312@gmail.com>
2016-03-10 17:30:26 -0600

    no message

--- src/net/andrew/utils/packets/CheckReceivePacket.java
+++
@@ -1,8 +0,0 @@
-package net.andrew.utils.packets;
-
-public class CheckReceivePacket {
-	private static Boolean newPacket = false;
-	public static Boolean checkNewPacket(){
-		return CheckReceivePacket.newPacket;
-	}
-}
